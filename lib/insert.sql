6a69d44300a3c6beec5ada39cc268d348967f819
Kohmmz <jeremiahkoome1@gmail.com>
2025-03-01 06:14:57 +0300

    Add SQL scripts for bears table creation and data insertion; update SQL queries for bear selection

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 50, "M", "black", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 90, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 40, "F", "white", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 40, "M", "white", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 30, "F", "black", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 70, "M", "white", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 50, "F", "black", "naive", 0);
+INSERT INTO bears VALUES (8,null, 4, "M", "black", "aggressive", 0);
